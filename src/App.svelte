a13f1b564104068bc59cbc11fd3213a6304b8eeb
Samuele de Tomasi <el3um4s@gmail.com>
2022-09-17 15:44:11 +0200

    Created a Svelte Project

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import "./css/tailwind.pcss";
+
+  const VERSION = "[VI]Version: {version} - built on {date}[/VI]";
+</script>
+
+<main>
+  <h1>Hello!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <p>Version: {VERSION}</p>
+</main>
+
+<style lang="postcss">
+  h1 {
+    @apply border-b-4;
+  }
+
+  h1:hover {
+    border-color: rgb(255, 136, 0);
+  }
+
+  a {
+    @apply p-1 rounded-lg;
+  }
+
+  a:hover {
+    border-bottom: 2px;
+    background-color: theme("colors.red.600");
+    color: theme("colors.gray.300");
+  }
+</style>
