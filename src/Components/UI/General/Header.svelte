2d0d8bc3838859d0f950631fd7cbe925a7d7eb93
Samuele De Tomasi <el3um4s@gmail.com>
2022-10-14 17:44:23 +0200

    close #30

--- src/Components/UI/General/Header.svelte
+++ src/Components/UI/General/Header.svelte
@@ -2,15 +2,24 @@
   import Logo from "../SVG/LOGO/Logo.svelte";
 
   import page from "../../../Stores/Page";
+  import password from "../../../Stores/Password";
 
   $: p = $page;
+
+  const goHome = () => {
+    page.set("Home");
+    password.set("");
+    window.history.replaceState({}, document.title, "/");
+  };
 </script>
 
 <header class="header-page {p}">
   <div
-    on:click={() => {
-      page.set("Home");
-      window.history.replaceState({}, document.title, "/");
+    on:click={goHome}
+    on:keydown={(e) => {
+      if (e.key === "Enter") {
+        goHome();
+      }
     }}
   >
     <Logo height="32px" />
